b73427fdc0faf00001d72b492915c6e8216eb763
Fiel <fielvioleta@yahoo.com>
2019-02-18 18:40:23 +0800

    Added dynamic data on index

--- resources/views/partials/guest/featured-product.blade.php
+++ resources/views/partials/guest/featured-product.blade.php
@@ -3,6 +3,7 @@
 		<h2 class="text-center">Featured Products</h2>
 	</div>
 	<div id="featureProducts" class="carousel slide" data-ride="carousel">
+	  
 	  <ol class="carousel-indicators">
 	    <li data-target="#featureProducts" data-slide-to="0" class="active"></li>
 	    <li data-target="#featureProducts" data-slide-to="1"></li>
@@ -11,132 +12,60 @@
 	  <div class="carousel-inner">
 		<div class="carousel-item active">
 			<div class="card-deck">
-			  <div class="card text-center">
-			    <img class="card-img-top" src="{{ asset('main/art-artistic-business-335257.jpg') }}" alt="Card image cap">
-			    <div class="card-body">
-			      <h5 class="card-title">Card title</h5>
-			      <p class="card-text">Category</p>
-			      <p class="card-text">USD 5</p>
-			      <div class="rating">
-			      	<span class="fa fa-star checked"></span>
-					<span class="fa fa-star checked"></span>
-					<span class="fa fa-star checked"></span>
-					<span class="fa fa-star"></span>
-					<span class="fa fa-star"></span>
-			      </div>		
-			    </div>
-			    <div class="card-footer">
-			      <button type="button" class="btn btn-primary btn-lg btn-block">Add to cart</button>
-			    </div>
-			  </div>
-			  <div class="card text-center">
-			    <img class="card-img-top" src="{{ asset('main/art-artistic-business-335257.jpg') }}" alt="Card image cap">
-			    <div class="card-body">
-			      <h5 class="card-title">Card title</h5>
-			      <p class="card-text">Category</p>
-			      <p class="card-text">USD 5</p>
-			    </div>
-			    <div class="card-footer">
-			      <button type="button" class="btn btn-primary btn-lg btn-block">Add to cart</button>
-			    </div>
-			  </div>
-			  <div class="card text-center">
-			    <img class="card-img-top" src="{{ asset('main/art-artistic-business-335257.jpg') }}" alt="Card image cap">
-			    <div class="card-body">
-			      <h5 class="card-title">Card title</h5>
-			      <p class="card-text">Category</p>
-			      <p class="card-text">USD 5</p>
-			      <div class="rating">
-			      	<span class="fa fa-star checked"></span>
-					<span class="fa fa-star checked"></span>
-					<span class="fa fa-star"></span>
-					<span class="fa fa-star"></span>
-					<span class="fa fa-star"></span>
-			      </div>
-			    </div>
-			    <div class="card-footer">
-			      <button type="button" class="btn btn-primary btn-lg btn-block">Add to cart</button>
-			    </div>
-			  </div>
-			  <div class="card text-center">
-			    <img class="card-img-top" src="{{ asset('main/art-artistic-business-335257.jpg') }}" alt="Card image cap">
-			    <div class="card-body">
-			      <h5 class="card-title">Card title</h5>
-			      <p class="card-text">Category</p>
-			      <p class="card-text">USD 5</p>
-			      <div class="rating">
-			      	<span class="fa fa-star checked"></span>
-					<span class="fa fa-star checked"></span>
-					<span class="fa fa-star checked"></span>
-					<span class="fa fa-star checked"></span>
-					<span class="fa fa-star checked"></span>
-			      </div>
-			    </div>
-			    <div class="card-footer">
-			      <button type="button" class="btn btn-primary btn-lg btn-block">Add to cart</button>
-			    </div>
-			  </div>
+			  	@foreach( $products as $product )
+					<div class="card text-center">
+						<img class="card-img-top" src="{{ asset('main/art-artistic-business-335257.jpg') }}" alt="Card image cap">
+						<div class="card-body">
+						  <h5 class="card-title">{{ $product->name }}</h5>
+						  <p class="card-text">{{ $product->category->name }}</p>
+						  <p class="card-text">USD {{ $product->price }}</p>
+						  <div class="rating">
+						  	<span class="fa fa-star checked"></span>
+							<span class="fa fa-star checked"></span>
+							<span class="fa fa-star checked"></span>
+							<span class="fa fa-star"></span>
+							<span class="fa fa-star"></span>
+						  </div>		
+						</div>
+						<div class="card-footer">
+						  <button type="button" class="btn btn-primary btn-lg btn-block">Add to cart</button>
+						</div>
+					</div>
+				@endforeach
 			</div>
 		</div>
 		<div class="carousel-item">
 			<div class="card-deck">
-			  <div class="card text-center">
-			    <img class="card-img-top" src="{{ asset('main/art-artistic-business-335257.jpg') }}" alt="Card image cap">
-			    <div class="card-body">
-			      <h5 class="card-title">Card title</h5>
-			      <p class="card-text">Category</p>
-			      <p class="card-text">USD 5</p>
-			    </div>
-			    <div class="card-footer">
-			      <button type="button" class="btn btn-primary btn-lg btn-block">Add to cart</button>
-			    </div>
-			  </div>
-			  <div class="card text-center">
-			    <img class="card-img-top" src="{{ asset('main/art-artistic-business-335257.jpg') }}" alt="Card image cap">
-			    <div class="card-body">
-			      <h5 class="card-title">Card title</h5>
-			      <p class="card-text">Category</p>
-			      <p class="card-text">USD 5</p>
-			    </div>
-			    <div class="card-footer">
-			      <button type="button" class="btn btn-primary btn-lg btn-block">Add to cart</button>
-			    </div>
-			  </div>
-			  <div class="card text-center">
-			    <img class="card-img-top" src="{{ asset('main/art-artistic-business-335257.jpg') }}" alt="Card image cap">
-			    <div class="card-body">
-			      <h5 class="card-title">Card title</h5>
-			      <p class="card-text">Category</p>
-			      <p class="card-text">USD 5</p>
-			    </div>
-			    <div class="card-footer">
-			      <button type="button" class="btn btn-primary btn-lg btn-block">Add to cart</button>
-			    </div>
-			  </div>
-			  <div class="card text-center">
-			    <img class="card-img-top" src="{{ asset('main/art-artistic-business-335257.jpg') }}" alt="Card image cap">
-			    <div class="card-body">
-			      <h5 class="card-title">Card title</h5>
-			      <p class="card-text">Category</p>
-			      <p class="card-text">USD 5</p>
-			    </div>
-			    <div class="card-footer">
-			      <button type="button" class="btn btn-primary btn-lg btn-block">Add to cart</button>
-			    </div>
-			  </div>
+			  
+			  @foreach( $products as $product )
+				  <div class="card text-center">
+				    <img class="card-img-top" src="{{ asset('main/art-artistic-business-335257.jpg') }}" alt="Card image cap">
+				    <div class="card-body">
+				      <h5 class="card-title">{{$product->name}}</h5>
+				      <p class="card-text">{{$product->category->name}}</p>
+				      <p class="card-text">USD {{$product->price}}</p>
+				    </div>
+				    <div class="card-footer">
+				      <button type="button" class="btn btn-primary btn-lg btn-block">Add to cart</button>
+				    </div>
+				  </div>
+			  @endforeach
+			  
 			 </div>
 		</div>
 	  </div>
+	  
 	  <a class="carousel-control-prev" href="#featureProducts" role="button" data-slide="prev">
 	    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
 	    <span class="sr-only">Previous</span>
 	  </a>
+
 	  <a class="carousel-control-next" href="#featureProducts" role="button" data-slide="next">
 	    <span class="carousel-control-next-icon" aria-hidden="true"></span>
 	    <span class="sr-only">Next</span>
 	  </a>
+
 	</div>
-	
 
 	<div class="spacer text-center">
 		<a class="btn btn-primary btn-lg" href="category.html" role="button">See More</a>
--- resources/views/partials/guest/sub-nav.blade.php
+++ resources/views/partials/guest/sub-nav.blade.php
@@ -1,24 +1,11 @@
 <div id="category-nav" class="bg-light sticky-top">
 	<nav role="navigation">
 		<ul class="nav nav-pills nav-fill">
-		  <li class="nav-item">
-		    <a class="nav-link" href="category.html">Category 1</a>
-		  </li>
-		  <li class="nav-item">
-		    <a class="nav-link" href="category.html">Category 2</a>
-		  </li>
-		  <li class="nav-item">
-		    <a class="nav-link" href="category.html">Category 3</a>
-		  </li>
-		  <li class="nav-item">
-		    <a class="nav-link" href="category.html">Category 4</a>
-		  </li>
-		  <li class="nav-item">
-		    <a class="nav-link" href="category.html">Category 5</a>
-		  </li>
-		  <li class="nav-item">
-		    <a class="nav-link" href="category.html">Category 6</a>
-		  </li>
+		  @foreach($categories as $category)
+			  <li class="nav-item">
+			    <a class="nav-link" href="category.html">{{ $category->name }}</a>
+			  </li>
+		  @endforeach
 		</ul>
 	</nav>
 </div>
