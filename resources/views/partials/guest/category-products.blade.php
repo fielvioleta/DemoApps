f9b58869841c54e5e99b24f9e3a8f97f8f455cf2
Fiel <fielvioleta@yahoo.com>
2019-02-24 21:51:22 +0800

    Fixed categories page

--- resources/views/partials/guest/category-products.blade.php
+++ resources/views/partials/guest/category-products.blade.php
@@ -4,17 +4,15 @@
 	</div>
 	<div class="card-deck spacer">
 		<div class="row products-list masonry">
+			
 			@foreach( $products as $index => $product )
-				{{-- @if( ++$index % 3 === 0 )
-					
-				@endif  --}}
 				<div class="card text-center">
 				    <a href="product.php">
-				    	<img class="card-img-top" height="300" src="/storage/products/{{$product->image_path}}" alt="Card image cap"></a>
+				    	<img class="card-img-top" @if( $index ==0 ) height="300" @endif src="/storage/products/{{$product->image_path}}" alt="Card image cap"></a>
 				    <div class="card-body">
-				      <h5 class="card-title">Card title</h5>
-				      <p class="card-text">Category</p>
-				      <p class="card-text">USD 5</p>
+				      <h5 class="card-title">{{$product->name}}</h5>
+				      <p class="card-text">{{$product->category->name}}</p>
+				      <p class="card-text">USD {{$product->price}}</p>
 				      <div class="rating">
 				      	<span class="fa fa-star checked"></span>
 						<span class="fa fa-star checked"></span>
@@ -25,47 +23,7 @@
 				    </div>
 				    <div class="card-footer">
 				      <button type="button" class="btn"><i class="fa-heart fa"></i></button>
-				      <button type="button" class="btn btn-primary"><i class="fa-shopping-cart fa"></i></button>
-				    </div>
-			  	</div>
-				<div class="card text-center">
-				    <a href="product.php">
-				    	<img class="card-img-top" src="/storage/products/{{$product->image_path}}" alt="Card image cap"></a>
-				    <div class="card-body">
-				      <h5 class="card-title">Card title Card title Card title</h5>
-				      <p class="card-text">Category</p>
-				      <p class="card-text">USD 5</p>
-				      <div class="rating">
-				      	<span class="fa fa-star checked"></span>
-						<span class="fa fa-star checked"></span>
-						<span class="fa fa-star checked"></span>
-						<span class="fa fa-star"></span>
-						<span class="fa fa-star"></span>
-				      </div>		
-				    </div>
-				    <div class="card-footer">
-				      <button type="button" class="btn"><i class="fa-heart fa"></i></button>
-				      <button type="button" class="btn btn-primary"><i class="fa-shopping-cart fa"></i></button>
-				    </div>
-			  	</div>
-				<div class="card text-center">
-				    <a href="product.php">
-				    	<img class="card-img-top" src="/storage/products/{{$product->image_path}}" alt="Card image cap"></a>
-				    <div class="card-body">
-				      <h5 class="card-title">Card title</h5>
-				      <p class="card-text">Category</p>
-				      <p class="card-text">USD 5</p>
-				      <div class="rating">
-				      	<span class="fa fa-star checked"></span>
-						<span class="fa fa-star checked"></span>
-						<span class="fa fa-star checked"></span>
-						<span class="fa fa-star"></span>
-						<span class="fa fa-star"></span>
-				      </div>		
-				    </div>
-				    <div class="card-footer">
-				      <button type="button" class="btn"><i class="fa-heart fa"></i></button>
-				      <button type="button" class="btn btn-primary"><i class="fa-shopping-cart fa"></i></button>
+				      <button type="button" class="btn btn-primary btn-add-cart" productValue="{{ $product }}"><i class="fa-shopping-cart fa"></i></button>
 				    </div>
 			  	</div>
 			@endforeach
