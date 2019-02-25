0314a7a24553c9df082e2f67105ee255ccf27ba5
Genevieve Madrazo <genevieve.madrazo@erni.ph>
2019-02-25 13:28:35 +0800

    floating alert box

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,6 +16,14 @@
 		@include('partials.guest.main-nav')
 	</div>
 	</div>
+
+
+	<div class="alert alert-warning alert-dismissible fade" role="alert">
+	  <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+	  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+	    <span aria-hidden="true">&times;</span>
+	  </button>
+	</div>
 	<!--nav-->
 
 	<!--slider main-->
